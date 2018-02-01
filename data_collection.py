d0225403f3b609f2b4f01ad58f1ab7769b74b5b5
Abhishaike <abhishaike@gmail.com>
2018-02-01 17:01:44 -0600

    bug fixes

--- data_collection.py
+++ data_collection.py
@@ -19,32 +19,33 @@ def collect_specific_hand(character_wanted, name):
         num_of_images = num_of_images - 1
         frame = vs.read()
         im = Image.fromarray(frame)
-        im.save("hand/%s/%s.jpg" % (character_wanted, name + str(num_of_images)))
-
+        im.save("hand/%s/%s.jpg" % (character_wanted, name + str(num_of_images))) #save it into created file
     print("Done!")
 
-def collect_not_hand():
+def collect_not_hand(name):
     vs = VideoStream().start()
     print("Starting data collection for non-hands in 5 seconds, get ready...")
     time.sleep(3)
     print("Collecting now!")
-    num_of_images = 500
+    num_of_images = 200
     while True and num_of_images != 0:
         num_of_images = num_of_images - 1
         frame = vs.read()
         im = Image.fromarray(frame)
-        im.save("not_hand/%s.jpg" % (num_of_images))
+        im.save("not_hand/%s.jpg" % (name + str(num_of_images)))
+    print("Done!")
 
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
+    # get both arguements
     ap.add_argument("-c", "--character", required=True,
                     help="Character Wanted, type 'None' if you're making negative images")
     ap.add_argument("-n", "--name", required=True,
                     help="Your name, used to save file.")
     args = vars(ap.parse_args())
     if args["character"] == 'None':
-        collect_not_hand()
+        collect_not_hand(args["name"].lower())
     else:
         collect_specific_hand(args["character"].lower(), args["name"].lower())
 
