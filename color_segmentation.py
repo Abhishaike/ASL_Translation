906c3f8a7ee7ba4e631ce6c9c4c5476cf5b148fb
briannoogin <briannoogin@yahoo.com>
2018-02-26 14:19:55 -0600

    deleted old code and preallocated memory

--- color_segmentation.py
+++ color_segmentation.py
@@ -16,12 +16,16 @@ from skimage.util import img_as_float
 # ap.add_argument("-k", "--kvalue", help="num segmentations")
 # args = vars(ap.parse_args())
 
-# start timer
-start = time.time()
+
 # load the image
 cap = cv2.VideoCapture(0)
-while(1):
-    ret,img = cap.read()
+# preallocate memory
+lower_blue = np.array([110, 50, 50])
+upper_blue = np.array([130, 255, 255])
+while(cap.isOpened):
+    # start timer
+    start = time.time()
+    _,img = cap.read()
     #img = cv2.imread(args["image"])
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = img[::2, ::2]
@@ -30,16 +34,12 @@ while(1):
     segments_fz = felzenszwalb(img, scale=1, sigma=0.9, min_size=1000)
     # segment color is the color of the hand
     segmentColor = None
-    has_color = False
     # convert to hsv for seperation
     hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     # set limits of blue
-    lower_blue = np.array([110, 50, 50])
-    upper_blue = np.array([130, 255, 255])
     rangeMask = cv2.inRange(hsv, lower_blue, upper_blue)
     # apply color mask
     img = cv2.bitwise_and(img,img,mask = rangeMask)
-
     # threshold the image to remove random blue noise from the image
     _,thresh = cv2.threshold(cv2.cvtColor(img,cv2.COLOR_BGR2GRAY),127,255,cv2.THRESH_BINARY)
     # finds the segment color when there are threshold values
@@ -49,10 +49,6 @@ while(1):
     indicies = np.where(segments_fz == segmentColor)
     img[indicies] = [255,255,255]
     img[not indicies] = [0,0,0]
-    # test if the correct segment color was chosen
-    #print(segmentColor)
-    # test if the whole hand was extracted
-    #cv2.imwrite("test2.png",img)
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     # calculated contours
     _,cnts,_ = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -66,7 +62,6 @@ while(1):
     x,y,w,h = cv2.boundingRect(cnts[0])
     cv2.rectangle(original_image,(x,y),(x+w,y+h),(0,255,0),2)
     cv2.imshow("Hand contours with bounding box",original_image)
-    cv2.imwrite("hand.png",original_image)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
